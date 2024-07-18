5264dd5eff65cd9a496387ff684db564acb02245
Nishan <nishz1072@gmail.com>
2024-07-18 13:47:29 +0530

    First commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  RedirectToSignIn
+} from '@clerk/nextjs'
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +23,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SignedIn>
+            <div className="mt-1 flex justify-end mr-5 w-full pr-5 h-14 container"> 
+            <UserButton showName/>
+            </div>
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+          <main>{children}</main>
+        </body>
     </html>
+    </ClerkProvider>
   );
 }
